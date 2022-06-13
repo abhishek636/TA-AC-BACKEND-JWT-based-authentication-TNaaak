a8f4a0e389aa4c942de0a145c1cb3514bb741d4d
abhishek <bhattabhishek636@gmail.com>
2022-06-13 11:59:11 +0530

    Assignment-1

--- block-BNaael/APIapplication/routes/index.js
+++ block-BNaael/APIapplication/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get("/dashboard", auth.verifyToken , (req,res) => {
+router.get("/api/dashboard", auth.verifyToken , (req,res) => {
   console.log(req.user);
   res.json({access: "protected resource"})
 })
