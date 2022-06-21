65023117bf762daca7b087e25f1585956edcb011
abhishek <bhattabhishek636@gmail.com>
2022-06-21 12:39:09 +0530

    Assignment-3

--- block-BNaaen/conduit/models/User.js
+++ block-BNaaen/conduit/models/User.js
@@ -5,6 +5,7 @@ var jwt = require('jsonwebtoken');
 
 var userSchema = new Schema(
   {
+    name:{type:String, required:true},
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
